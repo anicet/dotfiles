1611db1b991839ff258fc17bafb30bd4cdadbebf
anicet <anicet@anicet.com>
2013-11-21 13:07:25 +0100

    update

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 My dotfiles
 ===========
+
+  ./install.sh
---
+++ install.sh
@@ -0,0 +1,8 @@
+cd ~;
+
+ln -s ~/code/dotfiles/alias .alias
+ln -s ~/code/dotfiles/tmux.conf .tmux.conf
+ln -s ~/code/dotfiles/vimrc.after .vimrc.after
+ln -s ~/code/dotfiles/gitignore .gitignore
+
+cp ~/code/dotfiles/gitconfig .gitconfig
