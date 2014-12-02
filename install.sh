25d7753f8ebaca9618e85c0d063e6f1a0bee4b72
anicet <anicet@anicet.com>
2014-12-02 09:58:29 +0100

    update anicetrc

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 cd ~;
 
-ln -s ~/code/dotfiles/alias .alias
+ln -s ~/code/dotfiles/anicetrc .anicetrc
 ln -s ~/code/dotfiles/tmux.conf .tmux.conf
 ln -s ~/code/dotfiles/vimrc.after .vimrc.after
 ln -s ~/code/dotfiles/gitignore .gitignore
